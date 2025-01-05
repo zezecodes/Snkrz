884502e96da964411049656178cec961c27ff191
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2025-01-05 22:27:49 +0000

    First commit

---
+++ src/test/java/com/eclipsa/Snkrz/SnkrzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclipsa.Snkrz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnkrzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
